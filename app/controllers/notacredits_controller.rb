c254c457dff69524521f62d359f7113bae5851be
Percy Woeds <percywoeds@gmail.com>
2017-06-30 15:50:14 +0000

    abc

--- app/controllers/notacredits_controller.rb
+++ app/controllers/notacredits_controller.rb
@@ -234,10 +234,12 @@ Banco de CREDITO Cuenta Corriente soles : 191-2231128-0-45 CCI : 002191002231128
         
 
         credit_note = SUNAT::CreditNote.new(credit_note_data)
-       
+               
 
        if credit_note.valid?                       
            credit_note.to_pdf
+           
+           puts $lcFileName
            
            $lcFileName1=File.expand_path('../../../', __FILE__)+ "/"+$lcFileName              
            
