9f8d29a1663313d454ed350ef7763638510a1136
Percy Woeds <percywoeds@gmail.com>
2017-07-03 19:54:08 +0000

    a

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -172,7 +172,7 @@ Banco de CREDITO Cuenta Corriente soles : 191-2231128-0-45 CCI : 002191002231128
             case_49 = InvoiceGenerator.new(1, 3, 1, $lg_serie_factura).with_different_currency2
         
             puts $lcFileName 
-            $lcFileName1=File.expand_path('../../../', __FILE__)+ "/"+$lcFileName
+            $lcFileName1=File.expand_path('../../../../', __FILE__)+ "/"+$lcFileName
         
         else
             case_3 = InvoiceGenerator.new(1, 3, 1, $lg_serie_factura).with_igv2(true)
