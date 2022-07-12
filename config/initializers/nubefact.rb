a0ba2663f5fec041d287f32f6a2c54ce832215d4
percy woeds <percywoeds@gmail.com>
2022-07-12 10:04:54 -0500

    ma

--- config/initializers/nubefact.rb
+++ config/initializers/nubefact.rb
@@ -4,13 +4,13 @@ require "./lib/nubefact/nube_fact"
 #hidrotransp
 
 
-#      NubeFact.url_token = '5cd4f0f4-0aca-456f-880c-f826c2bbd742'
- #    NubeFact.api_token = 'f4ae1a4681e74dd280ea10214544f89135c7d04c6fb24c6381adaaca488cf48f'
+      NubeFact.url_token = '5cd4f0f4-0aca-456f-880c-f826c2bbd742'
+     NubeFact.api_token = 'f4ae1a4681e74dd280ea10214544f89135c7d04c6fb24c6381adaaca488cf48f'
 
 
 
 # ruta percywoeds22@yahoo.com
 
-      NubeFact.url_token = '3d346d0b-c542-4bca-8bd8-553e72b52546'
-      NubeFact.api_token = 'acbd0bb1050643c69d2ea0864045d7d49cfd0e9a9fc1434284f8f7ce615657fb'
+#      NubeFact.url_token = '3d346d0b-c542-4bca-8bd8-553e72b52546'
+ #     NubeFact.api_token = 'acbd0bb1050643c69d2ea0864045d7d49cfd0e9a9fc1434284f8f7ce615657fb'
 
